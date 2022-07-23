15bc2f09043e44052f87162bcf94b91ce497156c
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2022-07-23 12:33:22 -0700

    refactored main class

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,22 @@ public class Main {
     static final int type_Season = 3;
     static final int type_Episode = 4;
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ArrayList<TVShow> shows = new ArrayList<>();
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
+        ArrayList<TVShow> shows = Main.ExtractShowsFromDB();
+
+        Main.ExtractSeasonsFromDB(shows);
+        Main.ExtractEpisodesFromDB(shows);
+
+        for(TVShow show : shows) {
+            System.out.print(show.ToXML());
+        }
+    }
+
+    public static ArrayList<TVShow> ExtractShowsFromDB() throws SQLException {
+        ArrayList<TVShow> shows = new ArrayList<>();
+
         Connection conn = DriverManager.getConnection("jdbc:sqlite:media.db");
-
-
         Statement stmt = conn.createStatement();
         ResultSet cursor = stmt.executeQuery("SELECT id, metadata_type, title"
                 + " FROM metadata_items "
@@ -27,10 +37,14 @@ public class Main {
                 shows.add(new_show);
             }
         }
+        conn.close();
+        return shows;
+    }
 
-
-        stmt = conn.createStatement();
-        cursor = stmt.executeQuery("SELECT id, metadata_type, parent_id, title"
+    public static void ExtractSeasonsFromDB(ArrayList<TVShow> shows) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:media.db");
+        Statement stmt = conn.createStatement();
+        ResultSet cursor = stmt.executeQuery("SELECT id, metadata_type, parent_id, title"
                 + " FROM metadata_items "
                 + " WHERE metadata_type = " + type_Season);
 
@@ -49,37 +63,34 @@ public class Main {
                 }
             }
         }
+        conn.close();
+    }
 
-
-        stmt = conn.createStatement();
-        cursor = stmt.executeQuery("SELECT id, metadata_type, parent_id, title, duration, [index]"
+    public static void ExtractEpisodesFromDB(ArrayList<TVShow> shows) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:media.db");
+        Statement stmt = conn.createStatement();
+        ResultSet cursor = stmt.executeQuery("SELECT id, metadata_type, parent_id, title, duration, [index]"
                 + " FROM metadata_items "
                 + " WHERE metadata_type = " + type_Episode);
 
-            while(cursor.next()) {
-                int entry_ID = cursor.getInt("id");
-                int metadata_type = cursor.getInt("metadata_type");
-                int parent_ID = cursor.getInt("parent_id");
-                String title = cursor.getString("title");
-                int duration = cursor.getInt("duration");
-                int index = cursor.getInt("index");
-                //summary = row[6]
+        while(cursor.next()) {
+            int entry_ID = cursor.getInt("id");
+            int metadata_type = cursor.getInt("metadata_type");
+            int parent_ID = cursor.getInt("parent_id");
+            String title = cursor.getString("title");
+            int duration = cursor.getInt("duration");
+            int index = cursor.getInt("index");
+            //summary = row[6]
 
-                if(metadata_type == type_Episode) {
-                    Episode new_episode = new Episode(entry_ID, title, duration, index, "summary", parent_ID);
-                    for(TVShow show : shows){
-                        if(show.DoesEpisodeBelongInShow(new_episode)){
-                            break;
-                        }
+            if(metadata_type == type_Episode) {
+                Episode new_episode = new Episode(entry_ID, title, duration, index, "summary", parent_ID);
+                for(TVShow show : shows){
+                    if(show.DoesEpisodeBelongInShow(new_episode)){
+                        break;
                     }
                 }
             }
-
-
-        for(TVShow show : shows) {
-            System.out.print(show.ToXML());
         }
-
         conn.close();
     }
 }
